b655492fcf317a6ee58cafb146a87e1eef5066e0
AngelinaKZ <150364745+AngelinaKZ@users.noreply.github.com>
2024-02-23 14:44:44 -0800

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,11 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Git test");
-        System.out.println("Git test");
-        System.out.println("Git test");
-        System.out.println("Git test");
-        System.out.println("Git test");
+        System.out.println("Git test!");
+        System.out.println("Git test!");
+        System.out.println("Git test!");
+        System.out.println("Git test!");
+        System.out.println("Git test!");
     }
 
 
